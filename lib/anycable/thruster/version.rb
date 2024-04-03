38f8b4d3f579583a2c79a20f1a89574eb5a172bc
Vladimir Dementyev <palkan@hey.com>
2024-04-03 13:27:33 -0700

    [anycable] Prepare fork

---
+++ lib/anycable/thruster/version.rb
@@ -0,0 +1,16 @@
+require_relative "../../thruster/version"
+
+module AnyCable
+  module Thruster
+    VERSION = "0.1.2"
+
+    # Very basic validation to ensure the versions are in sync
+    if ::Thruster::VERSION.split(".").take(2) != VERSION.split(".").take(2)
+      raise "Minor or major version mismatch"
+    end
+
+    if ::Thruster::VERSION.split(".")[2].to_i > VERSION.split(".")[2].to_i
+      raise "Patch version mismatch: must be greater or equal to Thruster's patch version"
+    end
+  end
+end
