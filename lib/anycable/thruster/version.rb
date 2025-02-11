5ce939189de5e5b4734ca9d017e876d355c8c660
Vladimir Dementyev <dementiev.vm@gmail.com>
2025-02-10 18:39:24 -0800

    Upgrade to AnyCable 1.6.0-rc.2

--- lib/anycable/thruster/version.rb
+++ lib/anycable/thruster/version.rb
@@ -2,7 +2,7 @@ require_relative "../../thruster/version"
 
 module AnyCable
   module Thruster
-    VERSION = "0.1.11"
+    VERSION = "0.1.12"
 
     # Very basic validation to ensure the versions are in sync
     if ::Thruster::VERSION.split(".").take(2) != VERSION.split(".").take(2)
